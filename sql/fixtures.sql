28a5771c8a8862061006c8b2306bfd3a390e87d0
lin_strijdom <lindiestrijdom@gmail.com>
2025-06-12 09:21:56 +0200

    Update: README, farming.sql, fixtures.sql, schema .drawio and .png

--- sql/fixtures.sql
+++ sql/fixtures.sql
@@ -1,5 +1,5 @@
 -- vegetation
---months of the year
+-- months of the year
 INSERT INTO month(last_update_by, name, sort_order) values ('Luna', 'January', 1);
 INSERT INTO month(last_update_by, name, sort_order) values ('Luna', 'February', 2);
 INSERT INTO month(last_update_by, name, sort_order) values ('Luna', 'March', 3);
@@ -13,7 +13,7 @@ INSERT INTO month(last_update_by, name, sort_order) values ('Luna', 'October', 1
 INSERT INTO month(last_update_by, name, sort_order) values ('Luna', 'November', 11);
 INSERT INTO month(last_update_by, name, sort_order) values ('Luna', 'December',12);
 
---plant growth activities type
+-- plant growth activities type
 INSERT INTO plant_growth_activity_type(last_update_by, name, sort_order) VALUES ('Luna', 'Sprouting', 1);
 INSERT INTO plant_growth_activity_type(last_update_by, name, sort_order) VALUES ('Luna', 'Seeding', 2);
 INSERT INTO plant_growth_activity_type(last_update_by, name, sort_order) VALUES ('Luna', 'Vegetative', 3);
@@ -21,21 +21,21 @@ INSERT INTO plant_growth_activity_type(last_update_by, name, sort_order) VALUES 
 INSERT INTO plant_growth_activity_type(last_update_by, name, sort_order) VALUES ('Luna', 'Flowering', 5);
 INSERT INTO plant_growth_activity_type(last_update_by, name, sort_order) VALUES ('Luna', 'Ripening', 6);
 
---plant usage
+-- plant usage
 INSERT INTO plant_usage(last_update_by, name) VALUES ('Luna', 'Food Plant');
 INSERT INTO plant_usage(last_update_by, name) VALUES ('Luna', 'Fodder Plant');
 INSERT INTO plant_usage(last_update_by, name) VALUES ('Luna', 'Commercial Plant');
 
 
 -- electricity
---electricity line type
+-- electricity line type
 INSERT INTO electricity_line_type(last_update_by, name, sort_order, current_a, voltage_v) VALUES ('Ashleigh', 'Low Voltage', 1, 50, 230);
 INSERT INTO electricity_line_type(last_update_by, name, sort_order, current_a, voltage_v) VALUES ('Ashleigh', 'Medium Voltage', 2, 75, 11000);
 INSERT INTO electricity_line_type(last_update_by, name, sort_order, current_a, voltage_v) VALUES ('Ashleigh', 'High Voltage', 3, 100, 33000);
 INSERT INTO electricity_line_type(last_update_by, name, sort_order, current_a, voltage_v) VALUES ('Ashleigh', 'Extra-High Voltage', 4, 120, 365000);
 INSERT INTO electricity_line_type(last_update_by, name, sort_order, current_a, voltage_v) VALUES ('Ashleigh', 'Ultra-High Voltage', 5, 150, 800000 );
 
---electricity line condition type
+-- electricity line condition type
 INSERT INTO electricity_line_condition_type(last_update_by, name) VALUES ('Ashleigh', 'Working');   
 INSERT INTO electricity_line_condition_type(last_update_by, name) VALUES ('Ashleigh', 'Broken');    
 
@@ -63,7 +63,6 @@ INSERT INTO water_point_type (last_update_by, name) VALUES ('Polly', 'Water Tank
 INSERT INTO water_line_type (last_update_by, name, sort_order) VALUES ('Polly', 'River', 1);
 INSERT INTO water_line_type (last_update_by, name, sort_order) VALUES ('Polly', 'Stream', 2);
 
-
 -- readings
 INSERT INTO readings(last_update_by, name) VALUES ('Mondli', 'Moisture Testers');
 
@@ -87,15 +86,8 @@ INSERT INTO point_of_interest_type (last_update_by, name, sort_order) VALUES ('J
 INSERT INTO point_of_interest_type (last_update_by, name, sort_order) VALUES ('Jeff', 'Water point', 6);
 
 -- condition
-INSERT INTO "condition"  (last_update_by, name) VALUES ('Jeff', 'Fixed');
-INSERT INTO "condition"  (last_update_by, name) VALUES ('Jeff', 'Broken');
-
--- pole 
-INSERT INTO pole (last_update, last_update_by, installation_date, geometry, height, pole_material_id, pole_function_id) VALUES ('2023-10-11','Charles', '2023-10-11',ST_GeomFromText('POINT(0 0)', 4326),20,1,1);
-INSERT INTO pole (last_update, last_update_by, installation_date, geometry, height, pole_material_id, pole_function_id) VALUES ('2023-10-11','Charles', '2023-10-11',ST_GeomFromText('POINT(0 0)', 4326),10,1,2);
-INSERT INTO pole (last_update, last_update_by, installation_date, geometry, height, pole_material_id, pole_function_id) VALUES ('2023-10-11','Charles', '2023-10-11',ST_GeomFromText('POINT(0 0)', 4326),70,1,3);
-INSERT INTO pole (last_update, last_update_by, installation_date, geometry, height, pole_material_id, pole_function_id) VALUES ('2023-10-11','Charles', '2023-10-11',ST_GeomFromText('POINT(0 0)', 4326),80,1,2);
-INSERT INTO pole (last_update, last_update_by, installation_date, geometry, height, pole_material_id, pole_function_id) VALUES ('2023-10-11','Charles', '2023-10-11',ST_GeomFromText('POINT(0 0)', 4326),20,1,2);
+INSERT INTO condition (last_update_by, name) VALUES ('Jeff', 'Fixed');
+INSERT INTO condition (last_update_by, name) VALUES ('Jeff', 'Broken');
 
 -- pole_material 
 INSERT INTO pole_material (last_update_by, name) VALUES ('Charles', 'Metal');
@@ -108,8 +100,9 @@ INSERT INTO pole_function (id, last_update_by, "function") VALUES (2, 'Charles',
 INSERT INTO pole_function (id, last_update_by, "function") VALUES (3, 'Charles', 'Flag');
 INSERT INTO pole_function (id, last_update_by, "function") VALUES (4, 'Charles', 'Street lighting');
 
--- pole_condition
--- Having an error on pole_uuid and condition_uuid
---INSERT INTO pole_conditions (last_update_by, notes, "date", pole_uuid) VALUES ('Charles', 'Good', '10/11/2023', c52c5d28-d1aa-4780-b560-3dde03773374); 
-
-
+-- pole 
+INSERT INTO pole (last_update, last_update_by, installation_date, geometry, height, pole_material_id, pole_function_id) VALUES ('2023-10-11','Charles', '2023-10-11',ST_GeomFromText('POINT(0 0)', 4326),20,1,1);
+INSERT INTO pole (last_update, last_update_by, installation_date, geometry, height, pole_material_id, pole_function_id) VALUES ('2023-10-11','Charles', '2023-10-11',ST_GeomFromText('POINT(0 0)', 4326),10,1,2);
+INSERT INTO pole (last_update, last_update_by, installation_date, geometry, height, pole_material_id, pole_function_id) VALUES ('2023-10-11','Charles', '2023-10-11',ST_GeomFromText('POINT(0 0)', 4326),70,1,3);
+INSERT INTO pole (last_update, last_update_by, installation_date, geometry, height, pole_material_id, pole_function_id) VALUES ('2023-10-11','Charles', '2023-10-11',ST_GeomFromText('POINT(0 0)', 4326),80,1,2);
+INSERT INTO pole (last_update, last_update_by, installation_date, geometry, height, pole_material_id, pole_function_id) VALUES ('2023-10-11','Charles', '2023-10-11',ST_GeomFromText('POINT(0 0)', 4326),20,1,2);
