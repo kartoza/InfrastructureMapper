1e2045150150d5a87e80b7fdd7b89fe5a08afb1b
steynfrancois10-dev <steynfrancois10@gmail.com>
2025-11-21 16:01:34 +0200

    Updated sql

--- sql/15-pollution.sql
+++ sql/15-pollution.sql
@@ -1,51 +1,14 @@
 -- --------------------------------------Pollution--------------------------------------------
--- POLLUTION IMPACT
-CREATE TABLE IF NOT EXISTS pollution_impact (
-    id SERIAL NOT NULL PRIMARY KEY,
-    distance_from_source FLOAT NOT NULL,
-    notes TEXT,
-    date DATE NOT NULL,
-    last_update TIMESTAMP DEFAULT now() NOT NULL,
-    last_update_by TEXT NOT NULL,
-    uuid UUID UNIQUE NOT NULL DEFAULT gen_random_uuid(),
-    pollution_source_id  NOT NULL REFERENCES pollution_source (id),
-    waterbody_id NOT NULL REFERENCES waterbody_type (id),
-    pollutant_id NOT NULL REFERENCES pollutant (id),
-    impact_severity_id NOT NULL REFERENCES impact_severity (id)  
-);
-COMMENT ON TABLE pollution_impact IS 'Represents the pollution impact from pollution sources at a single location.';
-
-COMMENT ON COLUMN pollution_impact.id IS 'The unique pollution impact ID. This is the Primary Key.';
-
-COMMENT ON COLUMN pollution_impact.distance_from_source IS 'Distance from the observation point to the nearest pollution source in meters.';
-
-COMMENT ON COLUMN pollution_impact.notes IS 'Notes made by the observer at the pollution impact site.';
-
-COMMENT ON COLUMN pollution_impact.date IS 'When the observation was made.';
-
-COMMENT ON COLUMN pollution_impact.last_update IS 'The date that the last update was made (yyyy-mm-dd hh:mm:ss).';
-
-COMMENT ON COLUMN pollution_impact.last_update_by IS 'The name of the user responsible for the latest update.';
-
-COMMENT ON COLUMN pollution_impact.uuid IS 'Global unique identifier.';
-
-COMMENT ON COLUMN pollution_impact.pollution_source_id IS 'The foreign key which references the id from the pollution_source table.';
-
-COMMENT ON COLUMN pollution_impact.waterbody_id IS 'The foreign key which references the id from the waterbody_type table.';
-
-COMMENT ON COLUMN pollution_impact.pollutant_id IS 'The foreign key which references the id from the pollutant table.';
-
-COMMENT ON COLUMN pollution_impact.impact_severity_id IS 'The foreign key which references the id from the impact_severity table.';
 
 -- POLLUTION SOURCE
 CREATE TABLE IF NOT EXISTS pollution_source (
     id SERIAL NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
-    geometry GEOMETRY (Point, 32735),
+    geometry GEOMETRY (Point, 32734),
     last_update TIMESTAMP DEFAULT now() NOT NULL,
     last_update_by TEXT NOT NULL,
     uuid UUID UNIQUE NOT NULL DEFAULT gen_random_uuid(),
-    property_use_type_id NOT NULL REFERENCES property_use_type (id)
+    property_use_type_id INTEGER NOT NULL REFERENCES property_use_type (id)
 );
 COMMENT ON TABLE pollution_source IS 'Location of a pollution source in the area of interest.';
 
@@ -72,8 +35,8 @@ CREATE TABLE IF NOT EXISTS source_pollutant (
     last_update TIMESTAMP DEFAULT now() NOT NULL,
     last_update_by TEXT NOT NULL,
     uuid UUID UNIQUE NOT NULL DEFAULT gen_random_uuid(),
-    pollution_source_id NOT NULL REFERENCES pollution_source (id),
-    pollutant_id NOT NULL REFERENCES pollutant (id)
+    pollution_source_id INTEGER NOT NULL REFERENCES pollution_source (id),
+    pollutant_id INTEGER NOT NULL REFERENCES pollutant (id)
 );
 COMMENT ON TABLE source_pollutant IS 'Estimated concentration of pollutants.';
 
@@ -91,7 +54,7 @@ COMMENT ON COLUMN source_pollutant.last_update_by IS 'The name of the user respo
 
 COMMENT ON COLUMN source_pollutant.uuid IS 'Global Unique Identifier.';
 
-COMMENT ON COLUMN source_pollutant.pollution_source IS 'The foreign key which references the id from the pollution_source table.';
+COMMENT ON COLUMN source_pollutant.pollution_source_id IS 'The foreign key which references the id from the pollution_source table.';
 
 COMMENT ON COLUMN source_pollutant.pollutant_id IS 'The foreign key which references the id from the pollutant table.';
 
@@ -187,16 +150,39 @@ COMMENT ON COLUMN waterbody_type.last_update_by IS 'The name of the user respons
 
 COMMENT ON COLUMN waterbody_type.uuid IS 'Global Unique Identifier.';
 
+-- WATER QUALITY STATUS
+CREATE TABLE IF NOT EXISTS water_quality_status (
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    notes TEXT,
+    last_update TIMESTAMP DEFAULT now() NOT NULL,
+    last_update_by TEXT NOT NULL,
+    uuid UUID UNIQUE NOT NULL DEFAULT gen_random_uuid()
+);
+COMMENT ON TABLE water_quality_status IS 'Lookup table for water quality for waterbodies in the area of interest, e.g. "Good", "Poor".';
+
+COMMENT ON COLUMN water_quality_status.id IS 'The unique water qulity status ID. This is the Primary Key.';
+
+COMMENT ON COLUMN water_quality_status.name IS 'Condition of water in waterbody.';
+
+COMMENT ON COLUMN water_quality_status.notes IS 'Additional information on water quality.';
+
+COMMENT ON COLUMN water_quality_status.last_update IS 'The date that the last update was made (yyyy-mm-dd hh:mm:ss).';
+
+COMMENT ON COLUMN water_quality_status.last_update_by IS 'The name of the user responsible for the latest update.';
+
+COMMENT ON COLUMN water_quality_status.uuid IS 'Global Unique Identifier.';
+
 -- WATERBODY
 CREATE TABLE IF NOT EXISTS waterbody (
     id SERIAL NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
-    geometry GEOMETRY (geometry, 32734),
+    geometry geometry (Geometry, 32734),
     last_update TIMESTAMP DEFAULT now() NOT NULL,
     last_update_by TEXT NOT NULL,
     uuid UUID UNIQUE NOT NULL DEFAULT gen_random_uuid(),
-    waterbody_type_id NOT NULL REFERENCES waterbody_type (id),
-    water_quality_status_id NOT NULL REFERENCES water_quality_status (id)
+    waterbody_type_id INTEGER NOT NULL REFERENCES waterbody_type (id),
+    water_quality_status_id INTEGER NOT NULL REFERENCES water_quality_status (id)
 );
 COMMENT ON TABLE waterbody IS 'Location for waterbodies in the area of interest.';
 
@@ -216,26 +202,40 @@ COMMENT ON COLUMN waterbody.waterbody_type_id IS 'The foreign key which referenc
 
 COMMENT ON COLUMN waterbody.water_quality_status_id IS 'The foreign key which references the id from the water_quality_status table.';
 
--- WATER QUALITY STATUS
-CREATE TABLE IF NOT EXISTS water_quality_status (
+-- POLLUTION IMPACT
+CREATE TABLE IF NOT EXISTS pollution_impact (
     id SERIAL NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL,
+    distance_from_source FLOAT NOT NULL,
     notes TEXT,
+    date DATE NOT NULL,
     last_update TIMESTAMP DEFAULT now() NOT NULL,
     last_update_by TEXT NOT NULL,
     uuid UUID UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+    pollution_source_id  INTEGER NOT NULL REFERENCES pollution_source (id),
+    waterbody_id INTEGER NOT NULL REFERENCES waterbody_type (id),
+    pollutant_id INTEGER NOT NULL REFERENCES pollutant (id),
+    impact_severity_id INTEGER NOT NULL REFERENCES impact_severity (id)  
 );
-COMMENT ON TABLE water_quality_staus IS 'Lookup table for water quality for waterbodies in the area of interest, e.g. "Good", "Poor".';
+COMMENT ON TABLE pollution_impact IS 'Represents the pollution impact from pollution sources at a single location.';
 
-COMMENT ON COLUMN water_quality_status.id IS 'The unique water qulity status ID. This is the Primary Key.';
+COMMENT ON COLUMN pollution_impact.id IS 'The unique pollution impact ID. This is the Primary Key.';
 
-COMMENT ON COLUMN water_quality_status.name IS 'Condition of water in waterbody.';
+COMMENT ON COLUMN pollution_impact.distance_from_source IS 'Distance from the observation point to the nearest pollution source in meters.';
 
-COMMENT ON COLUMN water_quality_status.notes IS 'Additional information of water quality'.;
+COMMENT ON COLUMN pollution_impact.notes IS 'Notes made by the observer at the pollution impact site.';
 
-COMMENT ON COLUMN water_quality_status.last_update IS 'The date that the last update was made (yyyy-mm-dd hh:mm:ss).';
+COMMENT ON COLUMN pollution_impact.date IS 'When the observation was made.';
 
-COMMENT ON COLUMN water_quality_status.last_update_by IS 'The name of the user responsible for the latest update.';
+COMMENT ON COLUMN pollution_impact.last_update IS 'The date that the last update was made (yyyy-mm-dd hh:mm:ss).';
 
-COMMENT ON COLUMN water_quality_status.uuid IS 'Global Unique Identifier.';
+COMMENT ON COLUMN pollution_impact.last_update_by IS 'The name of the user responsible for the latest update.';
 
+COMMENT ON COLUMN pollution_impact.uuid IS 'Global unique identifier.';
+
+COMMENT ON COLUMN pollution_impact.pollution_source_id IS 'The foreign key which references the id from the pollution_source table.';
+
+COMMENT ON COLUMN pollution_impact.waterbody_id IS 'The foreign key which references the id from the waterbody_type table.';
+
+COMMENT ON COLUMN pollution_impact.pollutant_id IS 'The foreign key which references the id from the pollutant table.';
+
+COMMENT ON COLUMN pollution_impact.impact_severity_id IS 'The foreign key which references the id from the impact_severity table.';
